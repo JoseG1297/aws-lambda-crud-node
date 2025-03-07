56d3c74ce2b22653c4abee68144526965ba8f4e4
JoseG1297 <mordo1309@gmail.com>
2025-03-07 12:57:57 -0600

    npm i uuid

--- src/addTask.js
+++ src/addTask.js
@@ -1,4 +1,11 @@
+const {v4} = require('uuid');
+
 exports.addTask = async (event) => {
+
+    const { title, description} = event.body;
+    const creatdAt = new Date();
+    const id = v4();
+
     return {
         statusCode: 200,
         body: JSON.stringify({
