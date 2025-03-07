c63e275deecc27ae287f28583bb129822f8a8e1a
JoseG1297 <mordo1309@gmail.com>
2025-03-07 13:12:58 -0600

    conectando a dynamoDB Para crear una tarea

--- src/addTask.js
+++ src/addTask.js
@@ -1,16 +1,29 @@
 const {v4} = require('uuid');
+const AWS = require('aws-sdk');
 
 exports.addTask = async (event) => {
+
+    const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
     const { title, description} = event.body;
     const creatdAt = new Date();
     const id = v4();
 
+    const newTask ={
+        id,
+        title,
+        description,
+        creatdAt
+    }
+
+    await dynamoDB.put({
+        TableName: 'taskTable',
+        Item: newTask
+    }).promise()
+
     return {
         statusCode: 200,
-        body: JSON.stringify({
-        message: "Task created succesfuly",
-        }),
-    };
+        body: JSON.stringify(newTask)
+    }
 };
   
